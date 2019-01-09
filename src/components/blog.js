25958bdbde97350754b25b7af340451d7d8e3f4d
ColeWoodford <woodford.cole@gmail.com>
2019-01-08 19:24:32 -0800

    Added custom blog component.

---
+++ src/components/blog.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Post } from '../styledComponents/blog-post-sc';
+
+class Blog extends Component {
+	render() {
+		return (
+			<div>
+				<Post>
+					My Blog Post
+				</Post>
+			</div>
+		)
+	}
+}
+
+export default Blog;
